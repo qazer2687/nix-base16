4e3a14e5477d9db337f6b054ce05a5af2347f2e3
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-09-15 16:38:56 +0100

    fix

--- flake.nix
+++ flake.nix
@@ -1,8 +1,9 @@
 {
   outputs = {...}: {
     base16 = name:
-      builtins.getAttr name (builtins.listToAttrs (
-        map
+      let
+        scheme = builtins.getAttr name (builtins.listToAttrs (
+          map
           (file: let
             s = import (./. + "/schemes/${file}");
             n = builtins.replaceStrings [".nix"] [""] file;
@@ -11,6 +12,12 @@
             value = s;
           })
           (builtins.attrNames (builtins.readDir ./schemes))
-      ));
+        ));
+      in
+        builtins.mapAttrs (_: value: 
+          if builtins.isString value && builtins.substring 0 1 value == "#"
+          then builtins.substring 1 (-1) value
+          else value
+        ) scheme;
   };
 }
