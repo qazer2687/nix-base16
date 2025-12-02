fa01e3ddb299a36345808258c81e4371a5c2d266
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-12-02 00:25:09 +0000

    test

--- flake.nix
+++ flake.nix
@@ -2,9 +2,10 @@
   inputs = {};
 
   outputs = _: {
-    base16 = name: builtins.foldl' (acc: line:
-      let m = builtins.match "  (base[0-9A-Fa-f]{2}): \"#([0-9A-Fa-f]{6})\"" line;
-      in if m != null then acc // { ${builtins.head m} = builtins.elemAt m 1; } else acc)
+    base16 = name: 
+      builtins.foldl' (acc: line:
+        let m = builtins.match "base([0-9A-Fa-f]{2}): \"#?([0-9A-Fa-f]{6})\"" line;
+        in if m != null then acc // { ${"base" + builtins.head m} = builtins.elemAt m 1; } else acc)
       {} (builtins.filter builtins.isString (builtins.split "\n" (builtins.readFile ./schemes/${name}.yaml)));
   };
 }
