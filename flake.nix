55a142a7b6a6e8f0f420e010b6b1482ca7f74fc8
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-12-02 00:30:41 +0000

    test

--- flake.nix
+++ flake.nix
@@ -3,9 +3,11 @@
 
   outputs = _: {
     base16 = name: 
-      builtins.foldl' (acc: line:
-        let m = builtins.match "[ ]*base([0-9A-Fa-f]{2}): \"#([0-9A-Fa-f]{6})\"" line;
-        in if m != null then acc // { ${"base" + builtins.head m} = builtins.elemAt m 1; } else acc)
-      {} (builtins.filter builtins.isString (builtins.split "\n" (builtins.readFile ./schemes/${name}.yaml)));
+      builtins.foldl' 
+        (acc: line:
+          let m = builtins.match ".*base([0-9A-F]{2}): \"([^\"]+)\".*" line;
+          in if m != null then acc // { ${"base" + builtins.elemAt m 0} = builtins.elemAt m 1; } else acc)
+        {}
+        (builtins.filter builtins.isString (builtins.split "\n" (builtins.readFile ./schemes/${name}.yaml)));
   };
 }
